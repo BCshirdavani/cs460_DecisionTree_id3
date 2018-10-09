6166bd8341fd52c4f60714ec9507c2e983f0da05
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-10-09 15:04:31 -0700

    test/train data split
    used random mask to split the data sets.

--- practice_01.py
+++ practice_01.py
@@ -10,14 +10,15 @@ Created on Tue Oct  9 13:10:10 2018
 
 import pandas as pd
 import os
+import numpy as np
 
 #============================================================================
-#   import the data
+#----------------------------------------------------------   import the data
 print(os.getcwd())
 df = pd.read_csv('/Users/shymacbook/Documents/BC/cs460_ML/HW/cs460_DecisionTree_id3/credit.csv')
 
 #============================================================================
-#   check out the data
+# ----------------------------------------------------------  check out the data
 pd.set_option('display.max_columns', None)
 df.head()
 df.describe().transpose()
@@ -31,39 +32,46 @@ for x in contCols:
 df.age.unique()
 
 #============================================================================
-#   bucket the continuous variables
+#----------------------------------------------------------   bucket the continuous variables
 df2 = pd.read_csv('/Users/shymacbook/Documents/BC/cs460_ML/HW/cs460_DecisionTree_id3/credit.csv')
 # bucket the age bins
 ageBins = [18, 27, 33, 42, 100]
 ageLabels = ['19-27', '27-33', '33-42', '42-100']
 df2['ageBin'] = pd.cut(df['age'], bins=ageBins, labels=ageLabels)
-# df2['age'] = df2['age'].astype(str)
 # bucket the months_loan_duration bins
-# monLoanBins = [1, 12, 18, 24, 100]
-# monLoanLabels = ['1-12', '12-18', '18-24', '24-100']
-# df2['monthsLoanBin'] = pd.cut(df['months_loan_duration'], bins=monLoanBins, labels=monLoanLabels)
 df2['months_loan_duration'] = df2['months_loan_duration'].astype(str)
 # bucket the amount bins
 amtBins = [1, 1360, 2320, 3970, 20000]
 amtLabels = ['1-1360', '1360-2320', '2320-243970' ,'3970-20000']
 df2['amtBin'] = pd.cut(df['amount'], bins=amtBins, labels=amtLabels)
 # bucket the perfent of income bins
-# incPctBins = [0.5, 1.5, 2.5, 3.5, 4.5]
-# incPctLabels = ['0.5-1.5', '1.5-2.5', '2.5-3.5', '3.5-4.5']
-# df2['incPctBins'] = pd.cut(df['percent_of_income'], bins=incPctBins, labels=incPctLabels)
 df2['percent_of_income'] = df2['percent_of_income'].astype(str)
 # bucket the years at residence bins
-# yrResBins = [0.5, 1.5, 2.5, 3.5, 4.5]
-# yrResLabels = ['0.5-1.5', '1.5-2.5', '2.5-3.5', '3.5-4.5']
-# df2['yrResBins'] = pd.cut(df['years_at_residence'], bins=yrResBins, labels=yrResLabels)
 df2['years_at_residence'] = df2['years_at_residence'].astype(str)
 # bucket the existing loans bins
-# xLoansBins = [0.5, 1.5, 2.5, 3.5, 4.5]
-# xLoansLabels = ['0.5-1.5', '1.5-2.5', '2.5-3.5', '3.5-4.5']
-# df2['xLoansBins'] = pd.cut(df['existing_loans_count'], bins=xLoansBins, labels=xLoansLabels)
 df2['existing_loans_count'] = df2['existing_loans_count'].astype(str)
 # bucket the dependents bins
 df2['dependents'] = df2['dependents'].astype(str)
+
+
+#============================================================================
+#----------------------------------------------------------     subset test/train data
+# make a 90% mask to split the test data
+mskTrain = np.random.rand(len(df2)) > 0.1
+# mskTest = 1 - mskTrain
+dfTrain = df2[mskTrain]
+dfTest = df2[~mskTrain]
+dfTrain.describe()
+dfTest.describe()
+
+#============================================================================
+#----------------------------------------------------------     ...
+
+
+
+
+
+
 
 
 
